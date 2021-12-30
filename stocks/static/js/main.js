37ecac0ed028348064f00c274c660f57f7620967
lStrachanowski <lukasz.strachanowski@gmail.com>
2021-12-30 14:11:56 +0100

    Animation in toggling forms added

--- stocks/static/js/main.js
+++ stocks/static/js/main.js
@@ -3,13 +3,38 @@ let value = false;
 document.getElementById("registerForm").style.display = "none";
 let formsToggle = () =>{
     if (value == false){
-        document.getElementById("loginForm").style.display = "none";
+        // Login form
+        document.getElementById("loginForm").style.animation = "fadeOut 0.7s";
+        document.getElementById("caption").style.animation = "fadeOut 0.7s";
+        setTimeout(()=>{
+            document.getElementById("loginForm").style.display = "none";
+        },650);
+
+        setTimeout(()=>{
+        // Register form
+        document.getElementById("registerForm").style.animation = "fadeIn 0.7s";
+        document.getElementById("caption").style.animation = "fadeIn 0.7s";
         document.getElementById("registerForm").style.display = "flex";
         document.getElementById("caption").innerHTML  = "Register";
+        },650);
+
     }else{
-        document.getElementById("registerForm").style.display = "none";
-        document.getElementById("loginForm").style.display = "flex";
-        document.getElementById("caption").innerHTML  = "Login";
+        // Login form
+        document.getElementById("registerForm").style.animation = "fadeOut 0.7s";
+        document.getElementById("caption").style.animation = "fadeOut 0.7s";
+        setTimeout(()=>{
+            document.getElementById("registerForm").style.display = "none";
+        },650);
+
+        setTimeout(()=>{
+            // Register form
+            document.getElementById("loginForm").style.animation = "fadeIn 0.7s";
+            document.getElementById("caption").style.animation = "fadeIn 0.7s";
+            document.getElementById("loginForm").style.display = "flex";
+            document.getElementById("caption").innerHTML  = "Login";
+        },650);
+
+
     }
     value = !value;
 }
