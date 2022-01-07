b43b28a6db811b7cfa92d0d73c179ee93f848236
lStrachanowski <lukasz.strachanowski@gmail.com>
2022-01-07 01:17:43 +0100

    Reset password prompt created

--- stocks/static/js/main.js
+++ stocks/static/js/main.js
@@ -1,46 +1,82 @@
 // Is toggling login form to register form 
 let value = false;
-document.getElementById("registerForm").style.display = "none";
-let formsToggle = () =>{
-    let login = document.getElementById("loginForm");
-    let caption = document.getElementById("caption");
-    let register = document.getElementById("registerForm");
+let toggleValue = false;
+let login = document.getElementById("loginForm");
+let caption = document.getElementById("caption");
+let register = document.getElementById("registerForm");
+let prompt = document.getElementById("promptForm");
+register.style.display = "none";
+prompt.style.display = "none";
 
-    if (value == false){
+let formsToggle = () => {
+    if (value == false) {
         // Login form
         login.style.animation = "fadeOut 0.7s";
         caption.style.animation = "fadeOut 0.7s";
-        setTimeout(()=>{
-            // elements[0].style.display = "none";
+        setTimeout(() => {
             login.style.display = "none";
-        },650);
+        }, 650);
 
-        setTimeout(()=>{
-        // Register form
-        register.style.animation = "fadeIn 0.7s";
-        caption.style.animation = "fadeIn 0.7s";
-        register.style.display = "flex";
-        caption.innerHTML  = "Register";
-        },650);
+        setTimeout(() => {
+            // Register form
+            register.style.animation = "fadeIn 0.7s";
+            caption.style.animation = "fadeIn 0.7s";
+            register.style.display = "flex";
+            caption.innerHTML = "Register";
+        }, 650);
 
-    }else{
+    } else {
         // Login form
         register.style.animation = "fadeOut 0.7s";
         caption.style.animation = "fadeOut 0.7s";
-        setTimeout(()=>{
+        setTimeout(() => {
             register.style.display = "none";
-        },650);
+        }, 650);
 
-        setTimeout(()=>{
+        setTimeout(() => {
             // Register form
             login.style.animation = "fadeIn 0.7s";
             caption.style.animation = "fadeIn 0.7s";
             login.style.display = "flex";
-            caption.innerHTML  = "Login";
-        },650);
+            caption.innerHTML = "Login";
+        }, 650);
 
 
     }
     value = !value;
 }
 
+let promptToggle = () => {
+    if (toggleValue == false) {
+        // Login form
+        login.style.animation = "fadeOut 0.7s";
+        caption.style.animation = "fadeOut 0.7s";
+        setTimeout(() => {
+            login.style.display = "none";
+        }, 650);
+        setTimeout(() => {
+            // Prompt form
+            prompt.style.animation = "fadeIn 0.7s";
+            caption.style.animation = "fadeIn 0.7s";
+            caption.innerHTML = "Reset password";
+            prompt.style.display = "flex";
+        }, 650);
+    } else {
+        // Login form
+        caption.style.animation = "fadeOut 0.7s";
+        prompt.style.animation = "fadeOut 0.7s";
+        setTimeout(() => {
+            prompt.style.display = "none";
+        }, 650);
+
+        setTimeout(() => {
+            // Register form
+            login.style.animation = "fadeIn 0.7s";
+            caption.style.animation = "fadeIn 0.7s";
+            login.style.display = "flex";
+            caption.innerHTML = "Login";
+        }, 650);
+
+    };
+    toggleValue = !toggleValue;
+}
