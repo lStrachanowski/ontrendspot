fad4f21f8ece742a8ab43997594dff739c7a54de
lStrachanowski <lukasz.strachanowski@gmail.com>
2022-02-18 23:32:55 +0100

    Forms.py created and form models

--- stocks/forms.py
+++ stocks/forms.py
@@ -1,1 +1,15 @@
+import email
+from click import password_option
 from django import forms
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(max_length=254)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class RegisterForm(forms.Form):
+    name = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=254)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class ResetForm(forms.Form):
+    email = forms.EmailField(max_length=254)
