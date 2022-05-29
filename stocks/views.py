167a1f431f9f0cdef753ca37b43badc6f07bc9ea
lStrachanowski <lukasz.strachanowski@gmail.com>
2022-05-29 18:26:54 +0200

    Login form is showing now also user name error when input was not fill in

--- stocks/views.py
+++ stocks/views.py
@@ -13,14 +13,14 @@ from .analytics import read_stock_from_file, add_to_database, get_stock_from_db
 from .charts import candle_chart
 import pandas as pd
 from datetime import datetime
-from django.contrib.sites.shortcuts import get_current_site  
-from django.utils.encoding import force_bytes, force_text  
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
-from django.template.loader import render_to_string  
-from .token import account_activation_token  
-from django.contrib.auth.models import User  
-from django.core.mail import EmailMessage  
-from django.http import HttpResponse  
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .token import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.http import JsonResponse
 
@@ -37,10 +37,10 @@ def activate(request, uidb64, token):
         user.is_active = True
         user.save()
         # return redirect('home')
-        f = {'message_text':'Thank you for your email confirmation. Now you can login your account.'}
+        f = {'message_text': 'Thank you for your email confirmation. Now you can login your account.'}
         return render(request, 'stocks/message.html', context=f)
     else:
-        f = {'message_text':'Activation link is invalid!'}
+        f = {'message_text': 'Activation link is invalid!'}
         return render(request, 'stocks/message.html', context=f)
 
 
@@ -52,9 +52,9 @@ def reset(request, uidb64, token):
         except(TypeError, ValueError, OverflowError, User.DoesNotExist):
             user = None
         if user is not None and account_activation_token.check_token(user, token):
-            return render(request, 'stocks/change_password.html') 
+            return render(request, 'stocks/change_password.html')
         else:
-            f = {'message_text':'Reset link is invalid!'}
+            f = {'message_text': 'Reset link is invalid!'}
             return render(request, 'stocks/message.html', context=f)
     if request.method == "POST":
         if 'change_password_button' in request.POST:
@@ -65,61 +65,65 @@ def reset(request, uidb64, token):
                 u = User.objects.get(pk=uid)
                 u.set_password(password)
                 u.save()
-                f = {'message_text':'Password changed'}
+                f = {'message_text': 'Password changed'}
                 return render(request, 'stocks/message.html', context=f)
             else:
-                f = {'message_text':'Invalid form ??'}
-                return render(request, 'stocks/message.html', context=f)  
+                f = {'message_text': 'Invalid form ??'}
+                return render(request, 'stocks/message.html', context=f)
         if 'error_confirm_button' in request.POST:
-    	    return redirect('index')
-    
+            return redirect('index')
+
+
 def index(request):
     candle_chart('pkn', 30, False)
     candle_chart('pkp', 30, False)
-    context = {"day":20092021}
+    context = {"day": 20092021}
     return render(request, 'stocks/index.html', context)
 
+
 def login_auth(request):
-    if request.method == "POST" :
+    if request.method == "POST":
         if 'login_button' in request.POST:
             form = LoginForm(request.POST)
             if form.is_valid():
                 user_name = form.cleaned_data['user']
                 password = form.cleaned_data['password']
-                user = authenticate(request, username=user_name, password=password)
+                user = authenticate(
+                    request, username=user_name, password=password)
                 if user is not None:
-                    if user.is_active:   
+                    if user.is_active:
                         login(request, user)
                         response = redirect('/')
                         return response
                 else:
-                    f = {'message_text':'Invalid user, password or account was not activated.'}
-                    return render(request, 'stocks/message.html', context=f)   
+                    f = {
+                        'message_text': 'Invalid user, password or account was not activated.'}
+                    return render(request, 'stocks/message.html', context=f)
             else:
-                f = {'form':form}
-                return render(request, 'stocks/login.html', context=f)   
- 
+                f = {'form': form}
+                return render(request, 'stocks/login.html', context=f)
+
         if 'reset_button_confirmation' in request.POST:
             form = ResetEmail(request.POST)
             if form.is_valid():
                 email = form.cleaned_data['email']
                 try:
                     user = User.objects.get(email__exact=email)
-                    current_site = get_current_site(request)  
-                    message = render_to_string('stocks/account_reset_email.html', {  
-                    'user': user,  
-                    'domain': current_site.domain,  
-                    'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
-                    'token':account_activation_token.make_token(user),  })   
+                    current_site = get_current_site(request)
+                    message = render_to_string('stocks/account_reset_email.html', {
+                        'user': user,
+                        'domain': current_site.domain,
+                        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                        'token': account_activation_token.make_token(user), })
                     print(message)
-                    f = {'message_text':'Check mailbox, password reset link was sent.'}
+                    f = {'message_text': 'Check mailbox, password reset link was sent.'}
                     return render(request, 'stocks/message.html', context=f)
                 except User.DoesNotExist:
-                    f = {'message_text':"No account registred with this email"}
+                    f = {'message_text': "No account registred with this email"}
                     return render(request, 'stocks/message.html', context=f)
             else:
-                f = {'message_text':form.errors.as_text}
-                return render(request, 'stocks/message.html', context=f) 
+                f = {'message_text': form.errors.as_text}
+                return render(request, 'stocks/message.html', context=f)
 
         if 'error_confirm_button' in request.POST:
             return render(request, 'stocks/login.html')
@@ -127,50 +131,52 @@ def login_auth(request):
     if request.method == "GET":
         return render(request, 'stocks/login.html')
 
+
 def logout_view(request):
-    logout(request)   
+    logout(request)
     response = redirect('/')
     return response
+
 
 def register(request):
     if request.method == "GET":
         return render(request, 'stocks/register.html')
     if request.method == "POST":
-        if 'register_button' in request.POST :
-            form = RegisterForm(request.POST )
+        if 'register_button' in request.POST:
+            form = RegisterForm(request.POST)
             if form.is_valid():
                 name = form.cleaned_data['name']
                 email = form.cleaned_data['email']
                 password = form.cleaned_data['password']
                 try:
-                    user = User.objects.create_user(name, email,password)
+                    user = User.objects.create_user(name, email, password)
                     user.is_active = False
                     user.save()
-                    current_site = get_current_site(request)  
-                    mail_subject = 'Activation link has been sent to your email id'  
-                    message = render_to_string('stocks/account_activate_email.html', {  
-                        'user': user,  
-                        'domain': current_site.domain,  
-                        'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
-                        'token':account_activation_token.make_token(user),  
-                    })   
-                    # email_message = EmailMessage(  
-                    #             mail_subject, message, to=[email]  
-                    # )  
-                    # email_message.send()  
+                    current_site = get_current_site(request)
+                    mail_subject = 'Activation link has been sent to your email id'
+                    message = render_to_string('stocks/account_activate_email.html', {
+                        'user': user,
+                        'domain': current_site.domain,
+                        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                        'token': account_activation_token.make_token(user),
+                    })
+                    # email_message = EmailMessage(
+                    #             mail_subject, message, to=[email]
+                    # )
+                    # email_message.send()
                     print(message)
                     response = redirect('/confirmation')
                     return response
-                except Exception as e: 
-                    print('Failed: '+ str(e))
-                    if User.objects.filter(username = name).exists():
-                        f = {'message_text':'Username is already taken!'}
-                        return render(request, 'stocks/message.html', context=f)  
-            else: 
-                f = {'form':form}
+                except Exception as e:
+                    print('Failed: ' + str(e))
+                    if User.objects.filter(username=name).exists():
+                        f = {'message_text': 'Username is already taken!'}
+                        return render(request, 'stocks/message.html', context=f)
+            else:
+                f = {'form': form}
                 return render(request, 'stocks/register.html', context=f)
 
-    
+
 def confirmation(request):
     if request.method == "GET":
         return render(request, 'stocks/confirmation.html')
@@ -178,10 +184,12 @@ def confirmation(request):
         response = redirect('/')
         return response
 
-def update_name(request):
-    return JsonResponse({'user':request.user.username})
 
-@login_required(login_url='/login')      
+def update_name(request):
+    return JsonResponse({'user': request.user.username})
+
+
+@login_required(login_url='/login')
 def account(request):
     if request.method == "POST":
         form = FieldCheck(request.POST)
@@ -189,40 +197,45 @@ def account(request):
             if form.is_valid():
                 new_name = form.cleaned_data['new_name']
                 try:
-                    User.objects.get(username = new_name)
-                    f = {'message_text':'User exists, choose other name.'}
-                    return render(request, 'stocks/message.html', context=f)  
+                    User.objects.get(username=new_name)
+                    f = {'message_text': 'User exists, choose other name.'}
+                    return render(request, 'stocks/message.html', context=f)
                 except User.DoesNotExist:
-                    new_user = User.objects.get(username = request.user.username)
+                    new_user = User.objects.get(username=request.user.username)
                     new_user.username = new_name
                     new_user.save()
             else:
-                f = {'message_text':'Invalid username'}
-                return render(request, 'stocks/message.html', context=f)   
+                f = {'message_text': 'Invalid username'}
+                return render(request, 'stocks/message.html', context=f)
         if 'user_email_save' in request.POST:
             print('save email')
     return render(request, 'stocks/account.html')
 
+
 def edit(request):
     return render(request, 'stocks/edit.html')
+
 
 def list(request):
     return render(request, 'stocks/list.html')
 
+
 def daydetails(request, date):
     return render(request, 'stocks/daydetails.html')
 
+
 def stock(request, stockname):
     candle_chart(stockname, 90, True)
-    context = {"stock":Stock.objects.get(stock_symbol=stockname.upper())}
-    return render(request, 'stocks/stock.html',context )
+    context = {"stock": Stock.objects.get(stock_symbol=stockname.upper())}
+    return render(request, 'stocks/stock.html', context)
 
 
 def csrf_failure(request, reason=""):
     if 'error_confirm_button' in request.POST:
         return render(request, 'stocks/index.html')
-    f = {'message_text':'Something went wrong'}
-    return render(request, 'stocks/message.html', context=f) 
+    f = {'message_text': 'Something went wrong'}
+    return render(request, 'stocks/message.html', context=f)
+
 
 def page_not_found(request, exception=None):
     return render(request, 'stocks/404.html', status=404)
